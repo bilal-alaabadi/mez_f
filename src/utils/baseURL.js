e1c275ee7e5e8ea5c32f9698efaabbc3fa8b3ccd
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-06-12 17:52:00 +0400

    وصف التعديلات

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -5,5 +5,5 @@
 //     return 'http://localhost:5000';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-  return "https://pinkheart369.shop/api";
+  return "https://www.pinkheart369.shop/api";
 }
