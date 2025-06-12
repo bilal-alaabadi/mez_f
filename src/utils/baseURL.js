07dc31ea1a51a53890ef92bc04054114e8c82f9d
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-06-12 16:47:55 +0400

    تعديل الروابط للإنتاج

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,9 @@
 // export const getBaseUrl = () => {
 //     return 'https://pinkheart-b.vercel.app';//رابط الباك اند بدل 5000
 // }
+// export const getBaseUrl = () => {
+//     return 'http://localhost:5000';//رابط الباك اند بدل 5000
+// }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+  return "https://pinkheart369.shop/api";
 }
